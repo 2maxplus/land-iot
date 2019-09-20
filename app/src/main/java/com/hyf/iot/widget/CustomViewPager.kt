ab83e8f066dcc09d76989d0ad8474f115c7777ba
zhang <827023046@qq.com>
2019-09-20 18:21:34 +0800

    优化viewpager 嵌套viewpager 滑动不流畅

--- app/src/main/java/com/hyf/iot/widget/CustomViewPager.kt
+++ app/src/main/java/com/hyf/iot/widget/CustomViewPager.kt
@@ -4,16 +4,41 @@ import android.content.Context
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.View
+import kotlin.math.abs
 
 //com.hyf.iot.widget.CustomViewPager
 class CustomViewPager(ctx: Context, attrs: AttributeSet) : ViewPager(ctx, attrs) {
 
-    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
-        val `in` = super.onInterceptTouchEvent(ev)
-        if (`in`) {
-            parent.requestDisallowInterceptTouchEvent(true)
-            this.requestDisallowInterceptTouchEvent(true)
+    //    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+////        val `in` = super.onInterceptTouchEvent(ev)
+////        if (`in`) {
+////            parent.requestDisallowInterceptTouchEvent(true)
+////            this.requestDisallowInterceptTouchEvent(true)
+////        }
+//        return true
+//    }
+    override fun onInterceptHoverEvent(event: MotionEvent): Boolean {
+        val interceptTouchEvent = super.onInterceptTouchEvent(event)
+        var preX = 0f
+        if (event.action == MotionEvent.ACTION_DOWN);
+        run {
+            preX = event.x
         }
-        return false
+        run {
+            if (abs(event.x - preX) > 4) {
+                return true
+            } else {
+                preX = event.x
+            }
+        }
+        return interceptTouchEvent
+    }
+
+
+    override fun canScroll(v: View, checkV: Boolean, dx: Int, x: Int, y: Int): Boolean {
+        return if (v !== this && v is ViewPager) {
+            true
+        } else super.canScroll(v, checkV, dx, x, y)
     }
 }
